eb74e5b9343ed178e4e9ad781074fa793127b109
devchoigun <devchoigun@gmail.com>
2013-01-04 11:20:05 +0900

    test

--- myApp/admin/board/list.php
+++ myApp/admin/board/list.php
@@ -1,8 +1,8 @@
 <?
-	include "../config/lib.php"
+	include "../../config/lib.php"
 	
 	//DB에 연결하는 부분입니다. 항상 반복되는 부분이니 꼭 암기!!!
-	sql_connect($db_host, $db_user, $db_pass, $db_name);
+	$connect = sql_connect($db_host, $db_user, $db_pass, $db_name);
 	 
 	//게시판 목록보기에 필요한 각종 변수 초기값을 설정합니다.
 	$tablename="bbs"; //테이블 이름
@@ -13,10 +13,8 @@
 	$offset = $list_num*($page-1); //한 페이지의 시작 글 번호(listnum 수만큼 나누었을 때 시작하는 글의 번호)
 	 
 	//전체 글 수를 구합니다. (쿼리문을 사용하여 결과를 배열로 저장하는 일반적 인 방법)
-	$query="select count(*) from $tablename"; // SQL 쿼리문을 문자열 변수에 일단 저장하고
-	$result=mysql_query($query) or die (mysql_error()); // 위의 쿼리문을 실제로 실행하여 결과를 result에 대입
-	$row=mysql_fetch_row($result); //위 결과 값을 하나하나 배열로 저장합니다 .
-	$total_no=$row[0]; //배열의 첫번째 요소의 값, 즉 테이블의 전체 글 수를 저장합니다.
+	$query = sprintf("SELECT COUNT(number) AS cnt FROM $tablename"); // SQL 쿼리문을 문자열 변수에 일단 저장하고
+	$total_no = sql_total($query); //배열의 첫번째 요소의 값, 즉 테이블의 전체 글 수를 저장합니다.
 	 
 	//전체 페이지 수와 현재 글 번호를 구합니다.
 	$total_page=ceil($total_no/$list_num); // 전체글수를 페이지당글수로 나눈 값의 올림 값을 구합니다.
